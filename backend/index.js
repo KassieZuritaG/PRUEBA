1d48f309d4b63aa44a0551b7a3e4e4d3da6ce263
Rhyzard <rhyzard@MacBook-Pro-de-Rhyzard.local>
2023-08-17 03:16:57 -0500

    add

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const mainRoutes = require('./routes/mainRoutes');
 
 const app = express();
+app.use(cors());
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3100;
